12e237ca73e76fad0c8963c457bd84a40d0c6c5e
ramkumardevadoss <ramkumar.devadoss@city.ac.uk>
2024-01-03 18:57:56 +0000

    email implementation

--- src/main/java/com/inm429/ecommerce/Service/OrderService.java
+++ src/main/java/com/inm429/ecommerce/Service/OrderService.java
@@ -3,6 +3,7 @@ package com.inm429.ecommerce.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 import org.simplejavamail.email.Email;
 import org.simplejavamail.email.EmailBuilder;
@@ -10,8 +11,10 @@ import org.simplejavamail.mailer.Mailer;
 import org.simplejavamail.mailer.MailerBuilder;
 import org.simplejavamail.mailer.config.TransportStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
-
 import com.inm429.ecommerce.Model.Cart;
 import com.inm429.ecommerce.Model.Order;
 import com.inm429.ecommerce.Repository.CartRepository;
@@ -46,6 +49,9 @@ public class OrderService {
         if (optionalOrder.isPresent()) {
             Order order = optionalOrder.get();
             order.setShipped(shipped);
+            if(shipped) {
+            	sendNotificationEmail(orderId);
+            }
             return orderRepository.save(order);
         } else {
             throw new EntityNotFoundException("Order not found with id: " + orderId);
@@ -77,4 +83,34 @@ public class OrderService {
     	return resultList;
     }
     
+    public JavaMailSender sendNotificationEmail(Long orderId) {
+    	JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    	Optional<Order> order = orderRepository.findById(orderId);
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        
+        mailSender.setUsername("inm429cloudecommerce@gmail.com");
+        mailSender.setPassword("etdi vcfo rrbe nexm");
+        
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        
+        SimpleMailMessage message = new SimpleMailMessage(); 
+        message.setFrom("inm429cloudecommerce@gmail.com");
+        message.setTo(order.get().getCart().getUser().getEmail()); 
+        message.setSubject("Order Acknowledgement");
+        String emailMessage = "Dear " + order.get().getCart().getUser().getName()+" your order with order ID ";
+        emailMessage = emailMessage+order.get().getOrderId()+" containing item "
+        		+order.get().getCart().getProduct().getProductName()
+        		+" costing GBP "+order.get().getCart().getProduct().getProductPrice()
+        		+" is shipped to the address "+ order.get().getCart().getUser().getAddress();
+        message.setText(emailMessage);
+        mailSender.send(message);
+        
+        return mailSender;
+
+    }
 }
