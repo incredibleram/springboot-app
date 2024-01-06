e19df2368a6f0ff37fec44dcf15eba00b3ad974c
ramkumardevadoss <ramkumar.devadoss@city.ac.uk>
2024-01-06 17:23:19 +0000

    fix cors

--- src/main/java/com/inm429/ecommerce/Controller/CartController.java
+++ src/main/java/com/inm429/ecommerce/Controller/CartController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.inm429.ecommerce.Model.Cart;
 import com.inm429.ecommerce.Service.CartService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/cart")
 public class CartController {
 
